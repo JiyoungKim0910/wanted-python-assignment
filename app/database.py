67378eb184f8658e8e5c1ccc4a336b6b5e4f59d7
Jiyoung <jiyoung0910.kim@gmail.com>
2025-06-06 00:45:30 +0900

    [feature] Router 구현

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "sqlite:///./db.sqlite3"
+DB_URL = "sqlite:///./wantedDB.sqlite3"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False) 
